5e2e8b8776b1551abd398323b9e905f09580d2ec
Zahir Khan <zahir.khan1233@gmail.com>
2023-04-21 00:12:14 -0400

    Delete InterpreterDataType.java

--- InterpreterDataType.java
+++
@@ -1,9 +0,0 @@
-package ShankInterpreter;
-
-public abstract class InterpreterDataType {
-	
-	public abstract String ToString();
-	public abstract void FromString(String input);
-	protected abstract boolean modifyType();
-	
-}
